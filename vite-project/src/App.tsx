be1ee250a8fdea8a046ef48458134a039e01e369
Jonathan Leung <jonathan@kyleung.com>
2024-07-17 22:03:56 +0800

    Add board logic

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -14,15 +14,88 @@ class user {
   }
 }
 
+class cPiece {
+
+}
+
+class player {
+  name: string;
+  pieces: cPiece[];
+  time: number = 10 * 60;
+
+  constructor(name: string, pieces: cPiece[]) {
+    this.name = name;
+    this.pieces = pieces;
+  }
+}
+
+class game {
+  name: string = "test";
+  id: number;
+  white: player;
+  black: player;
+
+  constructor(id: number, white: player, black: player) {
+    this.id = id;
+    this.white = white;
+    this.black = black;
+  }
+}
+
+class cSquare {
+  piece: cPiece | null = null;
+  // cords from the top left
+  row: number;
+  col: number;
+  isWhite: boolean = true;
+
+  constructor(row: number, col: number, isWhite?: boolean) {
+    this.row = row;
+    this.col = col;
+
+    if (isWhite !== undefined) {
+      this.isWhite = isWhite;
+    }
+  }
+
+  getFile() : string {
+    if(this.isWhite) {
+      return LETTER_ARRAY[this.col];
+    }
+    return LETTER_ARRAY[NUM_COL - this.col - 1]; 
+  }
+
+  getRank() : number {
+    if(this.isWhite) {
+      return NUM_ROW - this.row;
+    }
+    return this.row + 1;
+  }
+}
+
+const NUM_COL = 8;
+const NUM_ROW = 8;
+const LETTER_ARRAY = 'abcdefgh';
+
 export default function App() {
   const [count, setCount] = useState(0)
   const [user, setUser] = useState()
+  
   const linkElementRef = useRef();
 
   let badCount: number = 0;
 
+  const initBoard: cSquare[][] = new Array<cSquare[]>(NUM_ROW);
+  for(let i = 0; i < NUM_ROW; i++) {
+    initBoard[i] = new Array<cSquare>(NUM_COL);
+    for(let j = 0; j < NUM_COL; j++) {
+      initBoard[i][j] = new cSquare(i, j);
+    }
+  }
+  const [board, setBoard] = useState(initBoard)
+
   useEffect(() => {
-    // TODO Install users
+    
   }, [])
 
   function handleClick() {
@@ -30,6 +103,13 @@ export default function App() {
     badCount++;
   }
 
+  function Board() {
+    return (
+        <div id="board">
+            { board.map((row) => row.map((square, i) => <div key={i}>{square.getFile()}{square.getRank()}</div>)) }
+        </div>
+    );
+  }
 
   return (
     <>
--- vite-project/src/Board.tsx
+++ vite-project/src/Board.tsx
@@ -1,47 +1,31 @@
 import { useState } from "react";
 
-class cbLoc {
-  x: number;
-  y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  toString(): string {
-    const letterArray = 'abcdefgh';
-    return `${letterArray[this.x]}${this.y}`;
-  }
-}
-
-class cbSquare {
-  isValid: boolean = false;
-  loc: cbLoc;
-
-  constructor(x: number, y: number) {
-    this.loc = new cbLoc(x, y);
-  }
-}
-
 export default function Board() {
   const numRows = 8;
   const numCols = 8;
 
-  const initCBBoard : cbSquare[][] = new Array<cbSquare[]>(numRows);
+  const initDisplayBoard : cSquare[][] = new Array<cSquare[]>(numRows);
+
+
+  // const chessBoard : map<string, >
 
   for(let i = 0; i < numRows; i++) {
-    initCBBoard[i] = new Array<cbSquare>(numCols);
+    initDisplayBoard[i] = new Array<cSquare>(numCols);
     for(let j = 0; j < numCols; j++) {
-      initCBBoard[i][j] = new cbSquare(i, j);
+      let x = numRows - 1 -i;
+      let y = j;
+      if (!isWhite) {
+
+      }
+      initDisplayBoard[i][j] = new cSquare(x, j);
     }
   }
 
-  const [cbBoard, setCBBoard] = useState(initCBBoard)
+  const [displayBoard, setdisplayBoard] = useState(initDisplayBoard)
 
   return (
-      <>
-          { cbBoard.map((row) => row.map((square, i) => <div key={i}>{square.loc.toString()}</div>)) }
-      </>
+      <div id="board">
+          { displayBoard.map((row) => row.map((square, i) => <div key={i}>{square.loc.toString()}</div>)) }
+      </div>
   );
 }
